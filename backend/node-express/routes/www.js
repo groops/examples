f91b485b788321a19390ee4275c763fbff8fff95
Brian Moeskau <brian@moeskau.com>
2014-07-22 22:32:35 -0500

    Implement /room/:id route with placeholder view

--- backend/node-express/routes/www.js
+++ backend/node-express/routes/www.js
@@ -17,8 +17,8 @@ module.exports = function(app){
   app.get('/main', function(req, res) {
     if (req.session.user) {
       request.get(apiRoot + 'rooms', function(err, response, data) {
-        if (err) {
-          console.log('error getting rooms: ' + err);
+        if (err || response.statusCode === 500) {
+          console.log('error getting rooms: ' + err || response.error);
           return res.send(500, err);
         }
         var rooms = _.sortBy(JSON.parse(data), function(room) {
@@ -40,7 +40,30 @@ module.exports = function(app){
 
   // Navigation to a specific room by id
   app.get('/room/:id', function(req, res) {
-    res.send(200, 'In room ' + req.params.id);
+    if (req.params.id) {
+      request.get(apiRoot + 'room/' + req.params.id, function(err, response, data) {
+        if (err || response.statusCode === 500) {
+          console.log('error getting room ' + req.params.id + ': ' + err || response.error);
+          return res.redirect('/main');
+        }
+        console.log(response.statusCode);
+        if (response.statusCode === 404) {
+          // Room id was not found, return to main view
+          console.log('room not found: ' + req.params.id);
+          return res.redirect('/main');
+        }
+        var room = JSON.parse(data);
+
+        res.render('room', {
+          title: 'Groop: ' + room.name,
+          room: room
+        });
+      });
+    }
+    else {
+      // No room id was passed, so just return to the main view
+      res.redirect('/main');
+    }
   });
 
   // Handle intro page form submission
@@ -53,8 +76,8 @@ module.exports = function(app){
       form: userInfo
     },
     function (err, response, data) {
-      if (err) {
-        console.log('error registering: ' + err);
+      if (err || response.statusCode === 500) {
+        console.log('error registering: ' + err || response.error);
         return res.send(500, err);
       }
       data = JSON.parse(data);
@@ -75,8 +98,8 @@ module.exports = function(app){
       }
     },
     function (err, response, data) {
-      if (err) {
-        console.log('error creating room: ' + err);
+      if (err || response.statusCode === 500) {
+        console.log('error creating room: ' + err || response.error);
         return res.send(500, err);
       }
       data = JSON.parse(data);
